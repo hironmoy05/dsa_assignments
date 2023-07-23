c64c5081eed2ea291ab6b42411182b2d3430a182
gauravzone <gauravdhar35@gmail.com>
2023-07-23 09:45:15 +0530

    Done remove duplicates from sorted array

---
+++ src/Arrays/BuildArray.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+import java.util.*;
+
+public class BuildArray {
+    public static void main(String[] args) {
+        int[] nums = {5, 0, 1, 2, 3, 4};
+
+        System.out.println(Arrays.toString(buildArray(nums)));
+    }
+
+    public static int[] buildArray(int[] nums) {
+        int[] ans = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = nums[nums[i]];
+        }
+
+        return ans;
+    }
+}
