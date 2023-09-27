cf1e7c0ba1cb2882db822a3b40eb1d131cf70b64
Hironmoy Dhar <hironmoy.dhar@repcard.com>
2023-09-27 07:50:42 +0530

    Done SortColors

---
+++ src/Arrays/Questions/SortColors.java
@@ -0,0 +1,45 @@
+package Arrays.Questions;
+
+import java.util.Arrays;
+
+public class SortColors {
+    public static void main(String[] args) {
+        int[] numbers = {2, 0, 2, 1, 1, 0};
+
+        sortColors(numbers);
+        System.out.println(Arrays.toString(numbers));
+    }
+
+    static void swap(int[] numbers, int start, int end) {
+        // swap
+        int temp = numbers[start];
+        numbers[start] = numbers[end];
+        numbers[end] = temp;
+    }
+
+    static void sortColors(int[] numbers) {
+        int start = 0;
+        int mid = 0;
+        int end = numbers.length - 1;
+
+        while (mid <= end) {
+            switch (numbers[mid]) {
+                case 0: {
+                    swap(numbers, start, mid);
+                    start++;
+                    mid++;
+                    break;
+                }
+                case 1: {
+                    mid++;
+                    break;
+                }
+                case 2: {
+                    swap(numbers, mid, end);
+                    end--;
+                    break;
+                }
+            }
+        }
+    }
+}
