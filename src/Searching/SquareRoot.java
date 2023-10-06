bbedce4f8c0df0d1132bfede57663d0bff508455
Hironmoy Dhar <hironmoy.dhar@repcard.com>
2023-10-06 07:38:03 +0530

    Done squareRoot

---
+++ src/Searching/SquareRoot.java
@@ -0,0 +1,12 @@
+package Searching;
+
+public class SquareRoot {
+    public static void main(String[] args) {
+        int x = 8;
+        System.out.println(squareRoot(x));
+    }
+
+    static int squareRoot(int x) {
+        return (int) Math.sqrt(x);
+    }
+}
